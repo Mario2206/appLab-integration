a4f25abe42d0268b66bb02abb7a608cb79c5db5f
Mathieu <mathieu.raimbault@hotmail.com>
2020-10-09 23:22:12 +0200

    :sparkles: Add canvas Anim

---
+++ src/canvas/Circle.ts
@@ -0,0 +1,72 @@
+import { exception } from "console";
+
+class Circle {
+    /**
+     * Canvas ref
+    */
+    private _canvas : HTMLCanvasElement;
+    private _context : CanvasRenderingContext2D | null
+
+    private _x : number
+    private _y : number
+    private _width : number
+    private _lineWidth : number | undefined 
+    private _dirX : number = 1 
+    private _dirY : number = 1 
+    private _speedX : number = 0
+    private _speedY : number = 0
+
+    private _color : string | CanvasGradient = "black" 
+
+    constructor(canvas : HTMLCanvasElement, width : number,  x : number, y : number, ) {
+        this._canvas = canvas
+        this._context = canvas.getContext("2d")
+        this._x = x
+        this._y = y
+        this._width = width
+        this._dirX = Math.random() > 0.5 ? 1 : -1
+        this._dirY = Math.random() > 0.5 ? 1 : -1
+    }
+
+    public set color(newColor : string | CanvasGradient) {
+        this._color = newColor
+    }   
+
+    public set lineWidth (width : number) {
+        this._lineWidth = width
+    }
+
+    public set speed ({speedX, speedY} : {speedX : number, speedY : number}) {
+        this._speedX = speedX
+            this._speedY = speedY
+    }
+
+    public draw() {
+
+        if(!this._context) throw new Error("No context")
+
+        this.defineDir()
+
+        this._x += this._dirX * this._speedX
+        this._y += this._dirY * this._speedY
+
+        this._context.beginPath()
+        this._context.arc(this._x, this._y,this._width,0, 2 * Math.PI, false)
+        this._context.stroke()
+        this._context.closePath()
+        this._context.strokeStyle = this._color;
+        this._context.lineWidth = this._lineWidth ?? this._width / 1.8
+
+
+    }
+
+    private defineDir() {
+        if(this._x >= this._canvas.width || this._x <= 0) this._dirX = -this._dirX 
+        if(this._y >= this._canvas.height || this._y <= 0) this._dirY = -this._dirY 
+
+    }
+
+
+}
+
+export default Circle
--- src/components/items/background-canvas.tsx
+++ src/components/items/background-canvas.tsx
@@ -1,4 +1,5 @@
 import  React, {useEffect} from 'react';
+import Circle from '../../canvas/Circle';
 import { getPercentageFromHeight, getPercentageFromWidth } from '../../helpers/percentage';
 
 interface IEBackgroundCanvas {
@@ -10,28 +11,46 @@ interface IEBackgroundCanvas {
 export default function BackgroundCanvas ({className, width, height} : IEBackgroundCanvas) {
 
     const canvasRef = React.createRef<HTMLCanvasElement>()
-    console.log(width);
+
+    function draw (circles : Array<Circle>, canvas : HTMLCanvasElement, ctx : CanvasRenderingContext2D) {
+            
+            ctx.clearRect(0,0,canvas.width, canvas.height)
+
+            circles.forEach((circle : Circle) => circle.draw())
+
+            requestAnimationFrame(()=>draw(circles,canvas, ctx));
+        
+    }
     
     useEffect(()=>{
+
+         const canvas = canvasRef.current
+         const ctx = canvas?.getContext("2d")
+
+        if(!canvas || !ctx) return
+
+         const circleWidth = getPercentageFromWidth(.7)
         
-        const canvas = canvasRef.current
-        const ctx = canvas?.getContext("2d")
+         const color1 = '#d4cdfd'
+         const color2 = "red"
 
-        if(ctx) {
+         const circle = new Circle(canvas, circleWidth,getPercentageFromWidth(10), getPercentageFromHeight(10))
+         circle.color = color1
+         circle.speed = {speedX  :.1, speedY :  .1 }
 
-            const width = getPercentageFromWidth(100)
-            const height = getPercentageFromHeight(7)
-            console.log(height);
-            
-            ctx.scale(1, 1)
-            ctx.fillStyle = 'red';
-            ctx.fillRect(10,10,width,height)
-        }
+         const circle2 = new Circle(canvas, circleWidth, getPercentageFromWidth(20), getPercentageFromHeight(70))
+         circle2.color = color2
+         circle2.speed = {speedX  :.2, speedY :  .2 }
 
+         const circle3 = new Circle(canvas, circleWidth, getPercentageFromWidth(80), getPercentageFromHeight(50))
+         circle3.color = color1
+         circle3.speed = {speedX  :.1, speedY :  .2 }
+
+        requestAnimationFrame(()=>draw([circle, circle2,circle3],canvas, ctx))
         
     }, [width,height])
 
     return (
-        <canvas ref={canvasRef} className={className} width={width} height={height}/>
+        <canvas ref={canvasRef} className={className} width={width} height={height} />
     )
 }
